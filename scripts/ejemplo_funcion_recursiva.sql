8d2c6b26a28d876c3daec6194c7740c23dcbcdb8
Fernando Pirani <feremp.i+dev@gmail.com>
2017-08-22 17:36:48 -0300

    Pedido: Ejemplo de funcion recursiva en postgresql

---
+++ scripts/ejemplo_funcion_recursiva.sql
@@ -0,0 +1,21 @@
+﻿-- Ejemplo función recursiva
+
+CREATE OR REPLACE FUNCTION concatenartelefonospersona(v_id_persona bigint, v_min_id_telefono bigint) RETURNS VARCHAR
+    AS $$ 
+    DECLARE v_id_tel BIGINT; v_cant_tels INTEGER; 
+    BEGIN 
+    SELECT MIN(tel0.id_telefono), COUNT(*)
+      INTO v_id_tel, v_cant_tels
+      FROM eaprender.telefonos tel0 
+     WHERE tel0.id_persona = v_id_persona 
+       AND tel0.id_telefono > COALESCE(v_min_id_telefono, 0);
+       
+    RETURN (SELECT tels.nro_telefono::VARCHAR 
+              || CASE WHEN v_cant_tels > 1 
+                      THEN '; ' || concatenartelefonospersona(v_id_persona, v_id_tel) 
+                      ELSE '' END 
+         FROM eaprender.telefonos tels 
+        WHERE tels.id_telefono = v_id_tel); END $$
+    LANGUAGE plpgsql
+    
+SELECT *, concatenarTelefonosPersona(pers.id_persona::BIGINT, NULL) telefonos_concatenados FROM eaprender.personas pers
